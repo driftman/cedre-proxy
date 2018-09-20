aa52012a2d06192944505fd55dbb66deeb2c8e40
Soufiane ELBAZ <elbaz.soufiane92@gmail.com>
2018-09-20 15:47:10 +0100

    Update Application.java

--- war/src/main/java/com/predic8/membrane/servlet/Application.java
+++ war/src/main/java/com/predic8/membrane/servlet/Application.java
@@ -1,15 +1,22 @@
 package com.predic8.membrane.servlet;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
 import com.predic8.membrane.servlet.embedded.MembraneServlet;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+		DataSourceAutoConfiguration.class, 
+		DataSourceTransactionManagerAutoConfiguration.class, 
+		HibernateJpaAutoConfiguration.class})
 public class Application {
-	
 	
 	@Bean
 	public ServletRegistrationBean<MembraneServlet> registrationBean() {
@@ -22,3 +29,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 }
+
